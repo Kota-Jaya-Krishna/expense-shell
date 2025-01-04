86710875a9462e9018e3a9677a47fa3d2fbdae3c
Kota-Jaya-Krishna <jayakrishnakota23@gmail.com>
2025-01-04 13:21:03 +0530

    My Sql Automation Script

--- MySql-Server.sh
+++ MySql-Server.sh
@@ -33,7 +33,7 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 CHECK_ROOT
 
-dnf install mysql-server -y &>>$LOG_FILE_NAME
+dnf install mysql-serve -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOG_FILE_NAME
